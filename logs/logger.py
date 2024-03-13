1e9c3eb17fee8ff6aa4bc4e19504a1e13a561f4d
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-03-13 17:36:36 -0300

    logs
    logs to api

---
+++ logs/logger.py
@@ -0,0 +1,61 @@
+import logging
+import os
+
+
+class CustomLogger:
+    def __init__(self, log_file_name='app.log'):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        # Diretório atual do script
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+
+        # Caminho do arquivo de log
+        log_file_path = os.path.join(script_dir, log_file_name)
+
+        # Configurar o formato do log
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s'
+        )
+
+        # Configurar o manipulador do log para escrever em um arquivo
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+
+        # Configurar o manipulador do log para imprimir na saída padrão
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(formatter)
+
+        # Adicionar os manipuladores ao logger
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def exception(self, message):
+        self.logger.exception(message)
+
+
+logger = CustomLogger()
+
+# Exemplo de uso
+if __name__ == '__main__':
+    logger = CustomLogger()
+
+    logger.info('Informação')
+    logger.warning('Aviso')
+    logger.error('Erro')
+    try:
+        # Simulando uma exceção
+        raise ValueError('Exceção simulada')
+    except Exception as e:
+        logger.exception('Exceção ocorrida: %s' % str(e))

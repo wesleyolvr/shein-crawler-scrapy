a151a757ef7105ce830e88c36b7a0c7efd3da82f
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-03-13 17:49:46 -0300

    Update product.py

--- api/schemas/product.py
+++ api/schemas/product.py
@@ -25,35 +25,34 @@ class ProductBase(BaseModel):
 
 
 class ProductCreate(ProductBase):
-    
-    @validator('imgs')
+
+    @validator("imgs")
     # pylint: disable=no-self-argument
     def convert_to_json(cls, value):
         """Converte a lista de URLs de imagens para uma string JSON."""
         return json.dumps(value)
 
-    @validator('store_code', 'is_on_sale', pre=True)
+    @validator("store_code", "is_on_sale", pre=True)
     # pylint: disable=no-self-argument
     def convert_to_integer(cls, value):
         """Converte para int."""
-        if value is None or value == '':
+        if value is None or value == "":
             return 0
         return int(value)
 
     @validator(
-        'price_real',
-        'price_us',
-        'discountPrice_price_real',
-        'discountPrice_us',
+        "price_real",
+        "price_us",
+        "discountPrice_price_real",
+        "discountPrice_us",
         pre=True,
     )
     # pylint: disable=no-self-argument
     def check_comma_and_convert(cls, value):
         """Verifica se o valor tem vírgula e converte para float."""
-        if ',' in str(value):
-            return float(value.replace(',', '.'))
+        if "," in str(value):
+            return float(value.replace(",", "."))
         return float(value)
-
 
 
 class ProductRead(ProductBase):

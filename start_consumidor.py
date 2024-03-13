a8f96f00b9bcd17be746ff98564953194726f70a
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-03-13 19:37:30 -0300

    Update start_consumidor.py

--- start_consumidor.py
+++ start_consumidor.py
@@ -1,20 +1,21 @@
 import multiprocessing
 
+from api.database.manager import ProdutoProcessor
 from config import KAFKA_TOPIC_IN
 
 
-def start_consumer():
-    from api.database.manager import ProdutoProcessor
-
-    processor_produtos = ProdutoProcessor(kafka_topic=KAFKA_TOPIC_IN)
+def start_consumer(kafka_topic):
+    processor_produtos = ProdutoProcessor(kafka_topic=kafka_topic)
     processor_produtos.processar_produto()
 
 
-def start_consumers(num_consumers):
+def start_consumers(num_consumers, kafka_topic):
     # Iniciar vários consumidores em processos separados
     processes = []
     for _ in range(num_consumers):
-        process = multiprocessing.Process(target=start_consumer)
+        process = multiprocessing.Process(
+            target=start_consumer, args=(kafka_topic,)
+        )
         process.start()
         processes.append(process)
 
@@ -28,4 +29,4 @@ if __name__ == '__main__':
     num_consumers = 2  # Por exemplo, iniciar 3 consumidores
 
     # Iniciar os consumidores em processos separados
-    start_consumers(num_consumers=num_consumers)
+    start_consumers(num_consumers=num_consumers, kafka_topic=KAFKA_TOPIC_IN)

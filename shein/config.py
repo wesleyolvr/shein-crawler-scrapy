a28abe4f3a0767f9606962eb8abf36dc4771bdb8
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-03-19 12:30:35 -0300

    config updated

--- shein/config.py
+++ shein/config.py
@@ -10,6 +10,6 @@ REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
 REDIS_EXPIRE_TIME = int(
     os.getenv('REDIS_EXPIRE_TIME', 345600)
 )  # 4 dias em segundos
-KAFKA_TOPIC_IN = os.getenv('KAFKA_TOPIC_IN', 'produtos_shein')
-KAFKA_TOPIC_OUT = os.getenv('KAFKA_TOPIC_OUT', 'produtos')
+KAFKA_TOPIC_products = os.getenv('KAFKA_TOPIC_products', 'products')
+KAFKA_TOPIC_url = os.getenv('KAFKA_TOPIC_url', 'url_to_spider')
 KAFKA_SERVERS = os.getenv('KAFKA_SERVERS', 'localhost:9093')

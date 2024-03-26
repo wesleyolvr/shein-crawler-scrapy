bb6210f413f7f96018782822f4324521225ebe16
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-03-26 19:41:00 -0300

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,22 @@ load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
 
 
 # Redis
-REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+REDIS_URL = os.getenv('REDIS_URL', 'redis://redis:6379')
 REDIS_EXPIRE_TIME = int(os.getenv('REDIS_EXPIRE_TIME', '3600'))
 
 # Kafka
-KAFKA_SERVERS = os.getenv('KAFKA_SERVERS', 'localhost:9093')
+KAFKA_SERVERS = os.getenv('KAFKA_SERVERS', 'kafka:9092')
 KAFKA_TOPIC_url = os.getenv('KAFKA_TOPIC_url', 'url_to_spider')
 KAFKA_TOPIC_products = os.getenv('KAFKA_TOPIC_products', 'products')
 KAFKA_TOPIC_grupo_id = os.getenv('KAFKA_TOPIC_grupo_id', 'my_consumer_group')
+CONSUMERS_API = int(os.getenv('CONSUMERS_SPIDER', '2'))
 
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
 
 DATABASE_URL = (
-    f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 )

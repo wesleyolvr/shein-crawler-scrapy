a1a96e86038154b7e898469e0ca118391e7575f9
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-03-26 19:44:56 -0300

    Update config.py

--- shein/config.py
+++ shein/config.py
@@ -6,10 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Obtém a URL do Redis e o tempo de expiração do ambiente
-REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+REDIS_URL = os.getenv('REDIS_URL', 'redis://redis:6379')
 REDIS_EXPIRE_TIME = int(
-    os.getenv('REDIS_EXPIRE_TIME', 345600)
-)  # 4 dias em segundos
+    os.getenv('REDIS_EXPIRE_TIME', 259200)
+)  # 3 dias em segundos
 KAFKA_TOPIC_products = os.getenv('KAFKA_TOPIC_products', 'products')
 KAFKA_TOPIC_url = os.getenv('KAFKA_TOPIC_url', 'url_to_spider')
-KAFKA_SERVERS = os.getenv('KAFKA_SERVERS', 'localhost:9093')
+KAFKA_SERVERS = os.getenv('KAFKA_SERVERS', 'kafka:9092')
+CONSUMERS_SPIDER = int(os.getenv('CONSUMERS_SPIDER', '3'))

790a10d9ad575a3f28a1b927f1c781c691ce36f1
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-03-26 22:39:28 -0300

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ KAFKA_SERVERS = os.getenv('KAFKA_SERVERS', 'kafka:9092')
 KAFKA_TOPIC_url = os.getenv('KAFKA_TOPIC_url', 'url_to_spider')
 KAFKA_TOPIC_products = os.getenv('KAFKA_TOPIC_products', 'products')
 KAFKA_TOPIC_grupo_id = os.getenv('KAFKA_TOPIC_grupo_id', 'my_consumer_group')
-CONSUMERS_API = int(os.getenv('CONSUMERS_SPIDER', '2'))
+CONSUMERS_API = int(os.getenv('CONSUMERS_API', '2'))
 
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')

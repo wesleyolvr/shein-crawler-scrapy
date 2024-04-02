22a8acbc2091324f74490ae3049fa107e8d89ed2
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-04-02 16:44:07 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,38 +1,40 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import FastAPI, HTTPException
 
 from api.schemas.product import ProductCreate, ProductRead
-from db.database import get_db
 from db.manager import DatabaseManager
+
+from config import DATABASE_URL
 
 app = FastAPI()
 
 
-@app.post('/products/', response_model=ProductRead)
-def create_product(
-    product: ProductCreate, db: DatabaseManager = Depends(get_db)
-):
-    db.create_product(product)
+@app.post("/products/", response_model=ProductRead)
+def create_product(product: ProductCreate):
+    db_manager = DatabaseManager(database_url=DATABASE_URL)
+    db_manager.create_product(product)
     return product
 
 
-@app.get('/products/{product_id}', response_model=ProductRead)
-def read_product(product_id: int, db: Session = Depends(get_db)):
-    db_manager = DatabaseManager(db)
+@app.get("/products/{product_id}", response_model=ProductRead)
+def read_product(product_id: int):
+    db_manager = DatabaseManager(database_url=DATABASE_URL)
     product = db_manager.get_product_by_id(product_id)
     if product is None:
-        raise HTTPException(status_code=404, detail='Product not found')
+        raise HTTPException(status_code=404, detail="Product not found")
     return product
 
 
-@app.get('/products/', response_model=list[ProductRead])
-def read_products(
-    skip: int = 0, limit: int = 10, db: DatabaseManager = Depends(get_db)
-):
-    db_manager = DatabaseManager(db)
-    products = db_manager.get_all_products()[skip : skip + limit]
+@app.get("/some_products/", response_model=list[ProductRead])
+def read_products(limit):
+    db_manager = DatabaseManager(database_url=DATABASE_URL)
+    products = db_manager.get_some_products(limit=limit)
     return products
 
 
-if __name__ == '__main__':
+@app.get("/")
+def home():
+    return {"Ola": "Mundo"}
+
+
+if __name__ == "__main__":
     pass
